7be8b94f80abc2e0e374eab0f7dc681e949af8dd
Rory Lariviere <rorydlar@gmail.com>
2021-02-24 20:26:43 -0600

    almost finished. have a bug due to using username not noticing seperate first and last name in promise. know a fix, will be patched at a later point in time

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
-import React, { useState } from 'react';
+import UserCard from './components/User';
+import formSchema from './validation/formSchema';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import * as yup from 'yup';
+
+////////// INITIAL STATES //////////
+////////// INITIAL STATES //////////
+////////// INITIAL STATES //////////
 
 // default form values EMPTY
 const initialFormValues = {
@@ -23,15 +28,74 @@ const initialDisabled = true;
 const initialUsers = [];
 
 export default function App() {
-////////// STATES //////////
-const [users, setUsers] = useState(initialUsers); // array of user objects
-const [formValues, setFormValues] = useState(initialFormValues); // object containing info passed in through user input
-const [formErrors, setFormErrors] = useState(initialFormErrors); // object containing yup errors if form is inproperly filled out
-const [disabled, setDisabled] = useState(initialDisabled); // used to disable submit button on form untill all yup conditions are met
+  ////////// STATES //////////
+  ////////// STATES //////////
+  ////////// STATES //////////
+  const [users, setUsers] = useState(initialUsers); // array of user objects
+  const [formValues, setFormValues] = useState(initialFormValues); // object containing info passed in through user input
+  const [formErrors, setFormErrors] = useState(initialFormErrors); // object containing yup errors if form is inproperly filled out
+  const [disabled, setDisabled] = useState(initialDisabled); // used to disable submit button on form untill all yup conditions are met 
+  console.log(users)
+  ////////// HELPERS //////////
+  ////////// HELPERS //////////
+  ////////// HELPERS //////////
+  const getUsers = () => { // gets user data from api to set to users state
+    axios.get(`https://reqres.in/api/users`)
+    .then(res => setUsers(res.data.data))
+    .catch(err => console.log(err))
+  }
+  const postNewUser = newUser => { // invokes form submition function and posts the resulting data to the provided api
+    axios.post(`https://reqres.in/api/users`, newUser) // takes 2 arguments, the api link and the callback being passed in
+         .then(res => setUsers([res.data.data, ...users])) // using spread attribute to expand the key/value pairs of the users array to include the new data we are passing to it
+         .catch(err => console.log(err))
+    setFormValues(initialFormValues); // resetting formValues back to its initial state so it can be used for new user data submissions via the form
+  }
+  ////////// EVENT HANDLERS //////////
+  ////////// EVENT HANDLERS //////////
+  ////////// EVENT HANDLERS //////////
+  const inputChange = (name, value) => { // validating user inputs with yup
+    yup.reach(formSchema, name).validate(value).then(() => setFormErrors({...formErrors, [name]: ''})) // takes name and formSchema to compare inputs with yup rules, if rules are met keep formErrors state same as initial
+       .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]})) // if any passed data does not pass validation, get the invalid key and return the error by updating formErrors accordingly
+    setFormValues({...formValues, [name]: value}) // updates formValues with validated data
+  }
+  const formSubmit = () => { // submits user input with new user data and makes a newUser object
+    const newUser = {
+      username: formValues.username.trim(), // trim is used to remove any white space potentially resulting from user input
+      email: formValues.email.trim(),
+      tos: ['tos'].filter(tos => formValues[tos]) // since tos has to be a check box and not radio button, must make array to filter through and apply users input
+    }
+    postNewUser(newUser); // invoking postNewUser to pass results of newUser into our post helper function
+  }
+  ////////// SIDE EFFECTS //////////
+  ////////// SIDE EFFECTS //////////
+  ////////// SIDE EFFECTS //////////
+  useEffect(()=>{ // side effect handler for getUsers axios call
+    getUsers();
+  }, [])
+
+  useEffect(()=>{ // side effect handler for formSchema validation check, adjusts status of 'disabled' every time 'formValues' changes
+    formSchema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
 
   return (
     <div className="App">
+      <header><h1>User Onboarding App</h1></header>
 
+      <Form
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+
+      {
+        users.map(userInfo => {
+          return (
+            <UserCard key={userInfo.id} details={userInfo} />
+          )
+        })
+      }
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,10 +1,74 @@
 import React from 'react';
 
 export default function Form(props) {
+    ////////// PROPS //////////
+    ////////// PROPS //////////
+    ////////// PROPS //////////
+    const { values, change, submit, disabled, errors } = props;
+    ////////// HELPER FUNCTIONS UTILIZING PROPS //////////
+    ////////// HELPER FUNCTIONS UTILIZING PROPS //////////
+    ////////// HELPER FUNCTIONS UTILIZING PROPS //////////
+    const onSubmit = evt => { // fires when a form is submitted
+        evt.preventDefault(); // prevents the event from refreshing the page when fired
+        submit(); // invokes the submit prop containing the formSubmit helper function from App.js
+    }
+    const onChange = evt => { // takes in data from input fields and creates an object
+        const { name, value, type, checked } = evt.target; // creating object containing the input data
+        const valueToUse = type === 'checkbox' ? checked : value; // ternary to check if checkbox is checked, and if so applying the value to it
+        change(name, valueToUse); // submitting the data from user input to inputChange event handler in App.js via the change prop
+    }
 
     return (
-        <div>
-            
-        </div>
+        <form className='form container' onSubmit={onSubmit} >
+            <div className='form-group submit'>
+                <h2>Add a User</h2>
+
+                {/* USES DISABLED PROP TO DETERMINE BUTTONS USABILITY */}
+                <button disabled={disabled} >submit</button>
+
+                {/* THIS IS WHERE THE VALIDATION ERRORS WILL BE RENDERED IF APPLICABLE */}
+                <div className='errors'>
+                    <div>{errors.username}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.tos}</div>
+                </div>
+            </div>
+
+            <div className='form-group inputs'>
+                <h4>General Information</h4>
+
+                {/* ////////// TEXT INPUTS ////////// */}
+                {/* ////////// TEXT INPUTS ////////// */}
+                {/* ////////// TEXT INPUTS ////////// */}
+                <label>Username&nbsp; {/* &nbsp; is non breaking space HTML entity. prevents new line break. not sure of its use here, look up at later date */}
+                    <input 
+                        value={values.username}
+                        onChange={onChange}
+                        name='username'
+                        type='text'
+                    />
+                </label>
+                <label>Email
+                    <input 
+                        value={values.email}
+                        onChange={onChange}
+                        name='email'
+                        type='text'
+                    />
+                </label>
+
+                {/* ////////// CHECKBOXES ////////// */}
+                {/* ////////// CHECKBOXES ////////// */}
+                {/* ////////// CHECKBOXES ////////// */}
+                <label>Read and Accept the Terms of Service&nbsp;
+                    <input
+                        type='checkbox'
+                        name='tos'
+                        onChange={onChange}
+                        checked={values.tos}
+                    />
+                </label>
+            </div>
+        </form>
     )
 }
